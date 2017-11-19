df4ef7d9213fb76aec7bc2f2b9c756fdb2b67bc7
Mohammad Hosein Khani Cosar Khizi <mh314k@gmail.com>
2017-11-19 22:51:20 +0330

    start of a bot life
    just deleting stickers that send not by admins

---
+++ bot.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater,MessageHandler,Filters
+from telegram import Bot,Update
+
+botUpdater = Updater(token="471374479:AAE7eprFR-9wRcArZQvEJiG2rupZGEamUe0")
+botDispatcher = botUpdater.dispatcher
+
+
+def sticker_recived(bot: Bot, update: Update):
+    fromAdmin = False
+    for admin in bot.get_chat_administrators(chat_id=update.message.chat_id):
+        if admin.user.id == update.message.from_user.id:
+            fromAdmin = True
+    if not fromAdmin:
+        bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id, timeout=1)
+
+
+stickerRecivedHandeler = MessageHandler(Filters.sticker, sticker_recived)
+botDispatcher.add_handler(stickerRecivedHandeler)
+botUpdater.start_polling()
+botUpdater.idle()
